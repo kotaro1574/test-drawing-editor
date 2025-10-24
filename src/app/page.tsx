61f55fb815d08d12519743ab70811ad11defb69f
kotaro1574 <shake1574@gmail.com>
2025-10-24 12:53:24 +0900

    fix: 文言修正

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import { DrawingCanvas } from "@/components/DrawingCanvas";
 export default function Home() {
   return (
     <main className="flex bg-blue-500 min-h-screen flex-col items-center justify-center">
-      <h1 className="text-2xl font-bold mb-4">Fabric.js Drawing Demo</h1>
+      <h1 className="text-2xl font-bold mb-4">絵を描こう！</h1>
       <DrawingCanvas />
     </main>
   );
